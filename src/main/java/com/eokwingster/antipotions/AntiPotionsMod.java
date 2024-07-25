67d4ea8dbbd85753b663e355d2a1f4875319cfc1
wingster <y1709175257@outlook.com>
2024-07-25 14:38:38 +0800

    init

---
+++ src/main/java/com/eokwingster/antipotions/AntiPotionsMod.java
@@ -0,0 +1,14 @@
+package com.eokwingster.antipotions;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(AntiPotionsMod.MODID)
+public class AntiPotionsMod
+{
+    public static final String MODID = "antipotions";
+
+    public AntiPotionsMod(IEventBus modEventBus) {
+
+    }
+}
